e3868ba1de1f024eab6eb83c27ffda86ee86b921
automationgroup <50216893+automationgroup@users.noreply.github.com>
2019-05-03 01:22:37 -0700

    Update colors.js

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,6 @@
 module.exports = {
-  PURPLE:  {hue: 180, saturation: 200},
-  BLUE: {hue: 144, saturation: 200},
+  PURPLE:  {hue: 75, saturation: 100},
+  BLUE: {hue: 70, saturation: 100},
   WHITE: {hue: 79, saturation: 7},
-  RED: {hue: 0, saturation: 200},
-  GREEN: {hue: 74, saturation: 200}
+  RED: {hue: 10, saturation: 100}
 };
